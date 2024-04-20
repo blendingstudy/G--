a4e9ac89f8ce3307851584111e2562be978ccb27
njy622 <njy622@naver.com>
2024-04-20 11:01:05 +0900

    20240420_dino jumping Game-developing...

--- Web-Mini-Games/app.py
+++ Web-Mini-Games/app.py
@@ -13,13 +13,8 @@ Session(app)
 def index():
     session['user_hearts'] = 3  # 유저 하트 초기화
     session['pc_hearts'] = 3    # PC 하트 초기화
-    # 카드 초기화 및 섞기
-    cards = ['A♠', 'A♠', 'K♣', 'K♣', 'Q♥', 'Q♥', 'J♦', 'J♦']
-    shuffle(cards)
-    session['cards'] = cards
-    session['flipped'] = []
-    session['start_time'] = time.time()
-    return render_template('index.html', cards=[None]*8)
+
+    return render_template('index.html')
 
 
 #############  가위바위보 게임  ##############################
@@ -56,174 +51,184 @@ def determine_winner(user, computer):
     
 
 
-#############  카드 뒤집기 게임  ##############################
-
-# @app.route('/Flip_the_card')
-# def flip():
-#      # 카드 초기화 및 섞기
-#     cards = ['A♠', 'A♠', 'K♣', 'K♣', 'Q♥', 'Q♥', 'J♦', 'J♦']
-#     shuffle(cards)
-#     session['cards'] = cards
-#     session['flipped'] = []
-#     session['start_time'] = time.time()
-#     return render_template('Games/Flip_the_card.html', cards=[None]*8)
-
-# @app.route('/Flip_the_card_play', methods=['POST'])
-# def flip_play():
-#     idx = int(request.form['index'])
-#     if idx in session['flipped']:
-#         return jsonify(success=False, message="이미 뒤집힌 카드입니다.")
-    
-#     session['flipped'].append(idx)
-#     if len(session['flipped']) == 2:
-#         idx1, idx2 = session['flipped']
-#         if session['cards'][idx1] == session['cards'][idx2]:
-#             if len(set(session['flipped'])) == 8:
-#                 total_time = int(time.time() - session['start_time'])
-#                 return jsonify(success=True, message="게임 성공!", time=total_time)
-#             return jsonify(success=True, message="일치!", cards=session['flipped'])
-#         else:
-#             session['flipped'] = []
-#             return jsonify(success=False, message="불일치!", index1=idx1, index2=idx2)
-#     return jsonify(card=session['cards'][idx], index=idx)
 
 
 ### 같은 그림의 카드 맞추기 게임 참고사이트(https://codethem.tistory.com/189)
 ## F5 누르면 게임 실행
 
-import turtle as t
-import random
-import time
+# import turtle as t
+# import random
+# import time
 
-# 클릭한 이미지 찾기 함수
-def find_card(x,y):
-    min_idx = 0
-    min_dis = 100
+# # 클릭한 이미지 찾기 함수
+# def find_card(x,y):
+#     min_idx = 0
+#     min_dis = 100
 
-    for i in range(16):
-        distance = turtles[i].distance(x,y)
-        if distance < min_dis:
-            min_dis = distance      # 우리가 클릭한 카드의 위치
-            min_idx = i             # 그 위치를 min_idx 담음
-    return min_idx
+#     for i in range(16):
+#         distance = turtles[i].distance(x,y)
+#         if distance < min_dis:
+#             min_dis = distance      # 우리가 클릭한 카드의 위치
+#             min_idx = i             # 그 위치를 min_idx 담음
+#     return min_idx
 
-# 정답오답 함수
-def score_update(m):
-    score_pen.clear()       # 클리어 스코프로 기존 값을 지워주고
-    score_pen.write(f"{m}   {score}점/{attempt}번 시도", False, "center", ("", 15))
-    # f스트링을 사용해서 매개 변수로 들어오는 m(메세지) 매개변수를 표시하고, 점수와 시도 횟수 표기
+# # 정답오답 함수
+# def score_update(m):
+#     score_pen.clear()       # 클리어 스코프로 기존 값을 지워주고
+#     score_pen.write(f"{m}   {score}점/{attempt}번 시도", False, "center", ("", 15))
+#     # f스트링을 사용해서 매개 변수로 들어오는 m(메세지) 매개변수를 표시하고, 점수와 시도 횟수 표기
 
-# 성공인지 실패인지 확인하는 함수
-def result(m):
-    t.goto(0, -60)              # 게임 오버 텍스트를 0,-60위치에 배치
-    t.write(m, False, "center", ("", 30, "bold"))
+# # 성공인지 실패인지 확인하는 함수
+# def result(m):
+#     t.goto(0, -60)              # 게임 오버 텍스트를 0,-60위치에 배치
+#     t.write(m, False, "center", ("", 30, "bold"))
 
-def play(x, y):
-    global click_num
-    global first_pick
-    global second_pick
-    global attempt
-    global score
+# def play(x, y):
+#     global click_num
+#     global first_pick
+#     global second_pick
+#     global attempt
+#     global score
 
-    if attempt == 12:               # 시도횟수가 12번이면
-        result("Game Over")
+#     if attempt == 12:               # 시도횟수가 12번이면
+#         result("Game Over")
 
-    else:
-        click_num += 1
-        # 클릭한 이미지 찾기
-        card_idx = find_card(x,y)
-        turtles[card_idx].shape(img_list[card_idx])
+#     else:
+#         click_num += 1
+#         # 클릭한 이미지 찾기
+#         card_idx = find_card(x,y)
+#         turtles[card_idx].shape(img_list[card_idx])
 
-        if click_num == 1:
-            first_pick = card_idx
-        elif click_num == 2:
-            second_pick = card_idx
-            click_num = 0
-            attempt += 1
+#         if click_num == 1:
+#             first_pick = card_idx
+#         elif click_num == 2:
+#             second_pick = card_idx
+#             click_num = 0
+#             attempt += 1
 
-            if img_list[first_pick]  == img_list[second_pick]:
-                score += 1
-                # 정답
-                score_update("정답")
-                if score == 8:
-                    result("Success")
-            else:
-                # 오답
-                score_update("오답")
-                # 오답인 경우 이미지가 다시 숨겨지도록 구현
-                turtles[first_pick].shape(defalut_img)
-                turtles[second_pick].shape(defalut_img)
-
-
+#             if img_list[first_pick]  == img_list[second_pick]:
+#                 score += 1
+#                 # 정답
+#                 score_update("정답")
+#                 if score == 8:
+#                     result("Success")
+#             else:
+#                 # 오답
+#                 score_update("오답")
+#                 # 오답인 경우 이미지가 다시 숨겨지도록 구현
+#                 turtles[first_pick].shape(defalut_img)
+#                 turtles[second_pick].shape(defalut_img)
 
 
 
-t.bgcolor("pink")
-t.setup(700,700)
-t.up()
-t.ht()
-t.goto(0,280)
-t.write("카드 매칭 게임", False, "center", ("", 30, "bold"))
-
-# 점수 펜 객체 생성
-score_pen = t.Turtle()  # 터틀객체 생성
-score_pen.up()          # 펜을 들여올려주고
-score_pen.ht()          # 하이드 터틀로 숨김
-score_pen.goto(0, 230)  # 제목보다 조금 아래쪽에 글자를 표시
 
 
-# 터틀 객체 생성
-turtles = []
-pos_x = [-210, -70, 70, 210]
-pos_y = [-250, -110, 30, 170]
+# t.bgcolor("pink")
+# t.setup(700,700)
+# t.up()
+# t.ht()
+# t.goto(0,280)
+# t.write("카드 매칭 게임", False, "center", ("", 30, "bold"))
 
-for x in range(4):
-    for y in range(4):
-        new_turtle = t.Turtle()
-        new_turtle.up()
-        new_turtle.color("pink")
-        new_turtle.speed(0)
-        new_turtle.goto(pos_x[x], pos_y[y])
-        turtles.append(new_turtle)
-
-defalut_img = 'Web-Mini-Games\static\images\Flip_the_card\default_img.gif'
-t.addshape(defalut_img)
+# # 점수 펜 객체 생성
+# score_pen = t.Turtle()  # 터틀객체 생성
+# score_pen.up()          # 펜을 들여올려주고
+# score_pen.ht()          # 하이드 터틀로 숨김
+# score_pen.goto(0, 230)  # 제목보다 조금 아래쪽에 글자를 표시
 
 
-# 준비된 이미지를 img를 img_list에 2번씩 삽입
-img_list = []
-for i in range(8):
-    img = f"Web-Mini-Games\static\images\Flip_the_card\img{i}.gif"
-    t.addshape(img)
-    img_list.append(img)
-    img_list.append(img)
+# # 터틀 객체 생성
+# turtles = []
+# pos_x = [-210, -70, 70, 210]
+# pos_y = [-250, -110, 30, 170]
 
-# 준비된 이미지를 img를 랜덤으로 섞음
-random.shuffle(img_list)
+# for x in range(4):
+#     for y in range(4):
+#         new_turtle = t.Turtle()
+#         new_turtle.up()
+#         new_turtle.color("pink")
+#         new_turtle.speed(0)
+#         new_turtle.goto(pos_x[x], pos_y[y])
+#         turtles.append(new_turtle)
 
-# 준비된 이미지를 화면에 일정히 배치 한 후 
-for i in range(16):
-    turtles[i].shape(img_list[i])
-
-# 3초간 쉬었다가
-time.sleep(3)
-
-# 카드 뒷면으로 뒤집음
-for i in range(16):
-    turtles[i].shape(defalut_img)
+# defalut_img = 'Web-Mini-Games\static\images\Flip_the_card\default_img.gif'
+# t.addshape(defalut_img)
 
 
-click_num = 0           # 클릭 횟수(매 2회 클릭마다 정답 체크)
-score = 0               # 점수 초기값
-attempt = 0             # 시도한 횟수
-first_pick = ""         # 첫 번째 클릭한 이미지
-second_pick = ""       # 두 번째 클릭한 이미지
+# # 준비된 이미지를 img를 img_list에 2번씩 삽입
+# img_list = []
+# for i in range(8):
+#     img = f"Web-Mini-Games\static\images\Flip_the_card\img{i}.gif"
+#     t.addshape(img)
+#     img_list.append(img)
+#     img_list.append(img)
+
+# # 준비된 이미지를 img를 랜덤으로 섞음
+# random.shuffle(img_list)
+
+# # 준비된 이미지를 화면에 일정히 배치 한 후 
+# for i in range(16):
+#     turtles[i].shape(img_list[i])
+
+# # 3초간 쉬었다가
+# time.sleep(3)
+
+# # 카드 뒷면으로 뒤집음
+# for i in range(16):
+#     turtles[i].shape(defalut_img)
 
 
-t.onscreenclick(play)   # t.onscreenclick(함수) 
-t.done()                # 프로그램 창이 자동으로 닫히지 않게 해줌
+# click_num = 0           # 클릭 횟수(매 2회 클릭마다 정답 체크)
+# score = 0               # 점수 초기값
+# attempt = 0             # 시도한 횟수
+# first_pick = ""         # 첫 번째 클릭한 이미지
+# second_pick = ""       # 두 번째 클릭한 이미지
 
 
+# t.onscreenclick(play)   # t.onscreenclick(함수) 
+# t.done()                # 프로그램 창이 자동으로 닫히지 않게 해줌
+
+
+### 같은 그림의 카드 맞추기 게임 참고사이트(https://codethem.tistory.com/189)
+## 웹으로 변경한 코드
+
+def init_game():
+    images = ['img0.png', 'img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png', 'img7.png'] * 2
+    random.shuffle(images)
+    session['cards'] = images
+    session['flipped_indices'] = []
+    session['score'] = 0
+
+@app.route('/flip')
+def flip_card():
+    init_game()  # 게임 초기화
+    return render_template('Games/Flip_the_card.html', cards=session['cards'])
+
+@app.route('/flip_card_play', methods=['POST'])
+def flip_card_play():
+    index = int(request.form['index'])
+    if len(session['flipped_indices']) == 2:
+        session['flipped_indices'] = []
+
+    if index not in session['flipped_indices']:
+        session['flipped_indices'].append(index)
+
+    if len(session['flipped_indices']) == 2:
+        idx1, idx2 = session['flipped_indices']
+        if session['cards'][idx1] == session['cards'][idx2]:
+            session['score'] += 1
+            message = "정답!"
+            success = True
+        else:
+            message = "오답!"
+            success = False
+            session['flipped_indices'] = []  # 불일치 시 카드 인덱스 리셋
+        return jsonify(success=success, message=message, cards=session['flipped_indices'])
+    return jsonify(success=False, message="카드를 뒤집으세요")
+
+@app.route('/dino')
+def dino():
+    return render_template('Games/dino-t-rex.html')
 
 
 if __name__ == '__main__':
